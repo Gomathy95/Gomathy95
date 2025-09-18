cd8aea55e42bd313ce6c050df511f67db3d92e4d
Gomathy95 <gomathya95@gmail.com>
2025-09-18 20:53:57 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.CONFIG = {
+  WAQI_TOKEN: "056ca09dbcd798da8af95cf2ff9a8bc6eecbd7f",
+  OWM_KEY: "65074012804cee478133b2d1ef3e721b",
+  GOOGLE_KEY: "AIzaSyA-8CTDisLXo_SLzKRQMv7ds4porl_cq9Q"
+};
